9190fff8f9abfd3e9b21d84f7904f64ff983da11
Dhanush-006 <122504741+Dhanush-006@users.noreply.github.com>
2023-01-12 22:36:38 +0530

    Add files via upload

---
+++ dhanush (bread).py
@@ -0,0 +1,13 @@
+a=int(input("fresh loaves"))
+b=int(input("day old leaves"))
+c=185
+d=60
+e=(c*d)/100
+f=a*b
+g=b*e
+
+
+print("regular price",c)
+print ("Total amount of fresh loaves:",f)
+print("total amount of old loaves:",g)
+print("total amount :",f+g)
